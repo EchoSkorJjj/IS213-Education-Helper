85a4179647873b03d4d29b4eeb75fe935bc9b56e
neilscallywag <neil.sharma.2022@scis.smu.edu.sg>
2024-02-28 15:00:58 +0800

    fix(fileprocessor): added validation for filesize over 5 mb

--- backend/simple/fileprocessor/src/ocr_processing.py
+++ backend/simple/fileprocessor/src/ocr_processing.py
@@ -6,30 +6,28 @@ import tempfile
 import subprocess
 from utilities import detect_locale
 
-# Determine the running environment
+# Setup based on the environment
 environment = os.getenv('ENVIRONMENT_MODE', 'development')
-
-# Setup logging based on the environment
-if environment.lower() == 'production':
-    logging_level = logging.INFO  # Less verbose for production
-else:
-    logging_level = logging.DEBUG  # More detailed logging for development
-
+logging_level = logging.INFO if environment.lower() == 'production' else logging.DEBUG
 logging.basicConfig(level=logging_level, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 def process_pdf_file(input_pdf_bytes, filename):
     logging.info(f"Starting OCR processing for file: {filename}")
-    temp_pdf_path = None
+    # Check the file size before processing
+    max_file_size = 5 * 1024 * 1024  # 5 MB
+    if len(input_pdf_bytes) > max_file_size:
+        logging.error(f"File {filename} exceeds the maximum allowed size of 5 MB.")
+        raise ValueError(f"File {filename} is too large to process.")
+
     try:
         input_stream = BytesIO(input_pdf_bytes)
         texts, temp_pdf_path = ocr_pdf(input_stream)
         metadata = generate_metadata(filename, temp_pdf_path, texts)
     except Exception as e:
-        # Log error without exposing sensitive details
         logging.error("Error in OCR processing: %s", e, exc_info=environment.lower() == 'development')
         raise
     finally:
-        # Cleanup temporary file securely
+        # Secure cleanup of the temporary file
         if temp_pdf_path and os.path.exists(temp_pdf_path):
             os.remove(temp_pdf_path)
             logging.debug("Temporary file removed.")
@@ -52,12 +50,14 @@ def ocr_pdf(input_stream):
 
 def extract_text_from_pdf(pdf_path):
     texts = []
-    pdf_file = open(pdf_path, 'rb')
-    reader = PdfReader(pdf_file)
-    for page_num, page in enumerate(reader.pages, start=1):
-        text = page.extract_text() or "Error extracting text"
-        texts.append({"pageId": page_num, "content": text})
-    pdf_file.close()
+    try:
+        pdf_file = open(pdf_path, 'rb')
+        reader = PdfReader(pdf_file)
+        for page_num, page in enumerate(reader.pages, start=1):
+            text = page.extract_text() or "Error extracting text"
+            texts.append({"pageId": page_num, "content": text})
+    finally:
+        pdf_file.close()
     return texts
 
 def generate_metadata(filename, pdf_path, texts):
