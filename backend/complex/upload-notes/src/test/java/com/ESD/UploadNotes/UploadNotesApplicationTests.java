ed53d2989a5917c4ee759ceb252cd35671985910
neilscallywag <neil.sharma.2022@scis.smu.edu.sg>
2024-02-29 17:27:39 +0800

    feat(uploadnotes): initialised maven for uplaod notes complex service

---
+++ backend/complex/upload-notes/src/test/java/com/ESD/UploadNotes/UploadNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ESD.UploadNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
