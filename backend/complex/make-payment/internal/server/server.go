046580f78b22f4b560ffcbe26cc92b3b04ebeec6
ztdevelops <yuezhengting@gmail.com>
2024-03-09 01:56:37 +0800

    feat(make-payment): checkout service implementation

--- backend/complex/make-payment/internal/server/server.go
+++ backend/complex/make-payment/internal/server/server.go
@@ -3,12 +3,18 @@ package payment
 import (
 	"context"
 	"log"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"github.com/EchoSkorJjj/IS213-Education-Helper/make-payment/pb"
 )
 
 type PaymentService interface {
-	MakePayment(ctx context.Context, req *make_payment.PaymentRequest) (*make_payment.PaymentResponse, error)
+	Checkout(ctx context.Context, req *make_payment.CheckoutRequest) (*make_payment.CheckoutResponse, error)
 }
 
 type Server struct {
@@ -19,7 +25,43 @@ func NewServer() *Server {
 	return &Server{}
 }
 
-func (s *Server) MakePayment(ctx context.Context, req *make_payment.PaymentRequest) (*make_payment.PaymentResponse, error) {
-	log.Print("hit!")
-	return &make_payment.PaymentResponse{Success: true}, nil
+func (s *Server) Checkout(ctx context.Context, req *make_payment.CheckoutRequest) (*make_payment.CheckoutResponse, error) {
+	var returnedErr error
+
+	email := req.Email
+
+	data := url.Values{}
+	data.Set("email", email)
+	dataReader := strings.NewReader((data.Encode()))
+	httpReq, err := http.NewRequest("GET", "http://payment-service:4567/checkout", dataReader)
+	if err != nil {
+		log.Printf("Failed to create new HTTP request: %v", err)
+		returnedErr = status.Errorf(codes.InvalidArgument, "Error creating request: %v", err)
+		return nil, returnedErr
+	}
+
+	httpReq.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+
+	httpResp, err := client.Do(httpReq)
+	if err != nil {
+		log.Printf("Failed to send HTTP request: %v", err)
+		returnedErr = status.Errorf(codes.Internal, "Error sending request: %v", err)
+		return nil, returnedErr
+	}
+	defer httpResp.Body.Close()
+
+	stripeRedirectUrl, err := httpResp.Location()
+	if err != nil {
+		log.Printf("Failed to extract Stripe Redirect URL from HTTP response: %v", err)
+		returnedErr = status.Errorf(codes.Internal, "Error getting Stripe Redirect URL: %v", err)
+		return nil, err
+	}
+
+	log.Printf("Successfully extracted Stripe Redirect URL: %v", stripeRedirectUrl.String())
+	return &make_payment.CheckoutResponse{Url: stripeRedirectUrl.String()}, nil
 }
