7144663e938229fe73401c6180be248b3a16886d
neilscallywag <neil.sharma.2022@scis.smu.edu.sg>
2024-03-08 11:11:50 +0800

    fix(uploadnotes): added controller advistor for 413 exception for large file sizes

---
+++ backend/complex/upload-notes/src/main/java/com/ESD/UploadNotes/advice/FileUploadExceptionAdvice.java
@@ -0,0 +1,18 @@
+package com.ESD.UploadNotes.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+import com.ESD.UploadNotes.utility.RequestExtractor;
+
+@ControllerAdvice
+public class FileUploadExceptionAdvice {
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<String> handleMaxSizeException(MaxUploadSizeExceededException exc) {
+        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("File too large! Kong ID: "+RequestExtractor.extractKongRequestId());
+    }
+}
