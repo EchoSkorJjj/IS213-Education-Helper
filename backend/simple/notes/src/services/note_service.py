2f7cd1165d35ff6cc42a266c5c6130742e07f3a1
ztdevelops <yuezhengting@gmail.com>
2024-03-22 18:38:00 +0800

    chore(notes): add service to retrieve multiple notes with pagination

--- backend/simple/notes/src/services/note_service.py
+++ backend/simple/notes/src/services/note_service.py
@@ -1,6 +1,6 @@
 import grpc
 import notes_pb2, notes_pb2_grpc
-from utils.s3utils import upload_to_s3, retrieve_from_s3
+from utils.s3utils import upload_to_s3, retrieve_from_s3, retrieve_multiple_from_s3
 from utils.logger import get_logger
 
 logger = get_logger(__name__)
@@ -27,3 +27,35 @@ class NoteServiceServicer(notes_pb2_grpc.NoteServiceServicer):
             context.set_details(f'Error retrieving note: {e}')
             context.set_code(grpc.StatusCode.INTERNAL)
             return notes_pb2.RetrieveNoteResponse()
+    
+    def RetrieveMultipleNotes(self, request, context):
+        try:
+            limit, offset, page = request.limit, request.offset, request.page
+            if offset == 0 and page == 0:
+                raise ValueError('Offset and page cannot be 0 at the same time')
+            if offset == 0: # offset takes precedence over page
+                offset = (page - 1) * limit
+            elif page == 0:
+                page = offset // max(1, limit) + 1
+            
+            file_contents = retrieve_multiple_from_s3(limit=limit, offset=offset)
+            logger.info(f"Retrieved {len(file_contents)} notes")
+
+            response = notes_pb2.RetrieveMultipleNotesResponse()
+            for file_content in file_contents:
+                response.notes.append(notes_pb2.Note(
+                    userId=file_content['user_id'],
+                    fileId=file_content['file_id'],
+                    fileContent=file_content['file_content']
+                ))
+            
+            response.count = len(file_contents)
+            response.nextPage = page + 1
+            return response
+        except Exception as e:
+            logger.error(f'Error retrieving multiple notes: {e}', exc_info=True)
+            context.set_details(f'Error retrieving multiple notes: {e}')
+            context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
+            return notes_pb2.RetrieveMultipleNotesResponse()
+
+
