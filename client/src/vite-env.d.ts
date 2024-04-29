ab6e1a817d92e5c276e4d73e30116c489256dd77
Jihoon_Song <69960711+EchoSkorJjj@users.noreply.github.com>
2024-04-29 13:56:09 +0800

    fix(frontend): added unsplash client id

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -4,12 +4,8 @@ interface ImportMetaEnv {
   readonly VITE_NODE_ENV: "development" | "production";
   readonly VITE_API_BASE_URL: string;
   readonly VITE_API_PROD_BASE_URL: string;
-  readonly VITE_TELEGRAM_BOT_API_KEY: string;
-  readonly VITE_GOOGLE_MAP_API_KEY: string;
   readonly VITE_GOOGLE_CLIENT_ID: string;
-  readonly VITE_LOCATIONIQ_ACCESS_TOKEN: string;
   readonly VITE_STORAGE_KEY: string;
-  readonly VITE_URL_ENVIRONMENT_PREFIX: string;
   readonly VITE_UNSPLASH_CLIENT_ID: string;
 }
 
